195bb6f8a3c92dd45765f396a1a288890443475a
Rik <gitaarik@posteo.net>
2024-06-23 17:48:33 +0200

    --wip-- [skip ci]

--- smb-mounter.py
+++ smb-mounter.py
@@ -4,163 +4,64 @@ import argparse
 import configparser
 import os
 import sys
-import uuid
 import asyncio
 import pyfuse3
 import errno
 import stat
-from getpass import getpass
-from smbprotocol.connection import Connection
-from smbprotocol.session import Session
-from smbprotocol.tree import TreeConnect
-from smbprotocol.open import (
-    CreateDisposition,
-    CreateOptions,
-    FileAttributes,
-    FilePipePrinterAccessMask,
-    ImpersonationLevel,
-    Open,
-    ShareAccess,
-)
-from smbprotocol.exceptions import SMBException, LogonFailure
+import smbclient
 import secretstorage
+from smbclient.shutil import copyfile
+from datetime import datetime
 
 
 class SmbFS(pyfuse3.Operations):
     def __init__(self, smb_server, smb_share, smb_username, smb_password):
         super().__init__()
-        self.smb_server = smb_server
-        self.smb_share = smb_share
+        self.smb_url = f"\\\\{smb_server}\\{smb_share}"
         self.smb_username = smb_username
         self.smb_password = smb_password
-        self.connection = None
-        self.session = None
-        self.tree = None
+        smbclient.ClientConfig(username=smb_username, password=smb_password)
 
     async def init(self):
-        self.connection = Connection(uuid.uuid4(), self.smb_server, 445)
-        self.connection.connect()
-        self.session = Session(self.connection, self.smb_username, self.smb_password)
-        self.session.connect()
-        self.tree = TreeConnect(self.session, self.smb_share)
-        self.tree.connect()
-
-    def open_path(self, path):
-
-        class OpenContextManager:
-
-            def __init__(self, tree, path):
-                self.tree = tree
-                self.path = path
-                self.file_obj = None
-
-            def __enter__(self):
-                self.file_obj = Open(self.tree, self.path)
-                self.file_obj.create(
-                    ImpersonationLevel.Impersonation,
-                    FilePipePrinterAccessMask.GENERIC_READ
-                    | FilePipePrinterAccessMask.GENERIC_WRITE,
-                    FileAttributes.FILE_ATTRIBUTE_NORMAL,
-                    ShareAccess.FILE_SHARE_READ | ShareAccess.FILE_SHARE_WRITE,
-                    CreateDisposition.FILE_OVERWRITE_IF,
-                    CreateOptions.FILE_NON_DIRECTORY_FILE,
-                )
-                return self.file_obj
-
-            def __exit__(self, exc_type, exc_value, traceback):
-                if exc_type is not None:
-                    print(
-                        f"An exception of type {exc_type} occurred with value {exc_value}"
-                    )
-
-                self.file_obj.close()
-
-        return OpenContextManager(self.tree, path)
+        # No need for explicit connection with smbclient
+        pass
 
     async def lookup(self, parent_inode, name, ctx=None):
         path = os.path.join(str(parent_inode), name)
-        with self.open_path(path) as file_obj:
-            attrs = file_obj.get_attributes()
+        attrs = smbclient.stat(f"{self.smb_url}\\{path}")
         return self._getattr(attrs)
-
-        try:
-            file_obj = Open(self.tree, path)
-            file_obj.create(ImpersonationLevel.Impersonation)
-            attrs = file_obj.get_attributes()
-            file_obj.close()
-            return self._getattr(attrs)
-        except SMBException:
-            raise pyfuse3.FUSEError(errno.ENOENT)
 
     async def getattr(self, inode, ctx=None):
         if inode == pyfuse3.ROOT_INODE:
             return {"st_mode": (stat.S_IFDIR | 0o755), "st_nlink": 2}
         path = str(inode)
-
-        with self.open_path(path) as file_obj:
-            attrs = file_obj.get_attributes()
-
+        attrs = smbclient.stat(f"{self.smb_url}\\{path}")
         return self._getattr(attrs)
-
-        try:
-            file_obj = Open(self.tree, path)
-            file_obj.create(ImpersonationLevel.Impersonation)
-            attrs = file_obj.get_attributes()
-            file_obj.close()
-            return self._getattr(attrs)
-        except SMBException:
-            raise pyfuse3.FUSEError(errno.ENOENT)
 
     def _getattr(self, attrs):
         mode = stat.S_IFREG | 0o644
-        if attrs["file_attributes"] & 0x10:  # Directory
+        if stat.S_ISDIR(attrs.st_mode):
             mode = stat.S_IFDIR | 0o755
         return {
             "st_mode": mode,
             "st_nlink": 1,
-            "st_size": attrs["end_of_file"],
-            "st_ctime": attrs["creation_time"].timestamp(),
-            "st_mtime": attrs["last_write_time"].timestamp(),
-            "st_atime": attrs["last_access_time"].timestamp(),
+            "st_size": attrs.st_size,
+            "st_ctime": attrs.st_ctime,
+            "st_mtime": attrs.st_mtime,
+            "st_atime": attrs.st_atime,
         }
 
     async def read(self, inode, off, size):
         path = str(inode)
-
-        with self.open_path(path) as file_obj:
-            data = file_obj.read(off, size)
-
-        return data
-
-        try:
-            file_obj = Open(self.tree, path)
-            file_obj.create(
-                ImpersonationLevel.Impersonation, desired_access=0x80000000
-            )  # FILE_READ_DATA
-            data = file_obj.read(off, size)
-            file_obj.close()
-            return data
-        except SMBException:
-            raise pyfuse3.FUSEError(errno.EIO)
+        with smbclient.open_file(f"{self.smb_url}\\{path}", mode="rb") as f:
+            f.seek(off)
+            return f.read(size)
 
     async def write(self, inode, off, buf):
         path = str(inode)
-
-        with self.open_path(path) as file_obj:
-            bytes_written = file_obj.write(buf, off)
-
-        return bytes_written
-
-        try:
-            file_obj = Open(self.tree, path)
-            file_obj.create(
-                ImpersonationLevel.Impersonation, desired_access=0x40000000
-            )  # FILE_WRITE_DATA
-            bytes_written = file_obj.write(buf, off)
-            file_obj.close()
-            return bytes_written
-        except SMBException:
-            raise pyfuse3.FUSEError(errno.EIO)
+        with smbclient.open_file(f"{self.smb_url}\\{path}", mode="r+b") as f:
+            f.seek(off)
+            return f.write(buf)
 
 
 def read_config():
@@ -243,41 +144,14 @@ async def main():
         password = get_password(args.mount)
         if not password:
             while True:
-                try:
-                    password = getpass(f"Enter password for {args.mount}: ")
-                except KeyboardInterrupt:
-                    print()
-                    sys.exit()
+                password = input(f"Enter password for {args.mount}: ")
+                # Try to connect and list root directory
+                smbclient.ClientConfig(username=smb_username, password=password)
+                smbclient.listdir(f"\\\\{smb_server}\\{smb_share}")
 
-                try:
-                    # Try to connect and list root directory
-                    conn = Connection(uuid.uuid4(), smb_server, 445)
-                    conn.connect()
-                    session = Session(conn, smb_username, password)
-                    session.connect()
-                    tree = TreeConnect(session, smb_share)
-                    tree.connect()
-                    root = Open(tree, "")
-                    root.create(
-                        ImpersonationLevel.Impersonation,
-                        FilePipePrinterAccessMask.GENERIC_READ
-                        | FilePipePrinterAccessMask.GENERIC_WRITE,
-                        FileAttributes.FILE_ATTRIBUTE_NORMAL,
-                        ShareAccess.FILE_SHARE_READ | ShareAccess.FILE_SHARE_WRITE,
-                        CreateDisposition.FILE_OVERWRITE_IF,
-                        CreateOptions.FILE_NON_DIRECTORY_FILE,
-                    )
-                    root.query_directory("*")
-                    root.close()
-                    tree.disconnect()
-                    session.disconnect()
-                    conn.disconnect()
-
-                    # If we get here, the password is correct
-                    save_password(args.mount, password)
-                    break
-                except LogonFailure:
-                    print("Incorrect password. Please try again.")
+                # If we get here, the password is correct
+                save_password(args.mount, password)
+                break
 
         fs = SmbFS(smb_server, smb_share, smb_username, password)
         fuse_options = set(pyfuse3.default_options)
